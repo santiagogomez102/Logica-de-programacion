1a03909e6356c9276c201db312a6c44863d6596c
santiagogomez102 <santiago.gomez102@outlook.com>
2024-04-20 19:22:22 -0500

    Intro + Primera parte
    38:44

---
+++ Primer ejercicio.py
@@ -0,0 +1,34 @@
+# https://www.python.org/
+
+# Comentario en una linea
+
+"""
+Esto tambien es 
+un comentario 
+en varias líneas
+"""
+
+'''
+Esto tambien es 
+un comentario 
+en varias líneas
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante" # por convención, no afecta funcionalidad
+
+my_int: int = 1              # : int es para dar contexto, no afecta funcionalidad
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+print("¡Hola, Python!")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
